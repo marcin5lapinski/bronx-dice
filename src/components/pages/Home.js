2edbfa57b724769ae00be40163ff79393fa685eb
unknown <marcin.lapinski05@gmail.com>
2021-11-23 17:46:52 +0100

    create new player page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,24 +3,24 @@ import img from "../../images/bronx-dice_logo.png"
 
 const Home = () => {
     return (
-        <div class="container">
-  <div class="sub-container">
-    <img src={img} alt="bronx-dice-logo" />
-    <div class="buttons-box">
-      <a href="/new-player">
-        <button class="btn nps-btn">New player</button>
-      </a>
-      <a href="/stats">
-        <button class="btn nps-btn">Statistics</button>
-      </a>
-    </div>
-    <div class="new-game-btn-box">
-        <a href="/pre-game">
-          <button class="btn ng-btn">New game</button>
-        </a>
-      </div>
-  </div>
-</div>
+        <div className="container">
+            <div className="sub-container">
+                <img src={img} alt="bronx-dice-logo" />
+                 <div className="buttons-box">
+                    <a href="/new-player">
+                        <button className="btn nps-btn">New player</button>
+                     </a>
+                    <a href="/stats">
+                        <button className="btn nps-btn">Statistics</button>
+                     </a>
+                 </div>
+                <div class="new-game-btn-box">
+                    <a href="/pre-game">
+                        <button className="btn ng-btn">New game</button>
+                    </a>
+                 </div>
+             </div>
+        </div>
     );
 }
 
--- src/components/pages/NewPlayer.js
+++ src/components/pages/NewPlayer.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const NewPlayer = () => {
     return (
-        <div>New player page</div>
+        <div class="container">
+            <a href="/"><button class="back-btn"></button></a>
+            <div class="sub-container">
+                <form class="new-player--form">
+                    <input type="text" name="name" placeholder="Enter your name/nickname" class="new-player--input" minlength="2" maxlength="12" />
+                     <button class="btn save-player--btn">Save</button>
+                 </form>
+             </div>
+        </div>
     );
 }
 
