d25a211333059b5639783ac250772c63428c1af6
unknown <marcin.lapinski05@gmail.com>
2021-11-23 20:36:20 +0100

    create stats page

--- src/components/pages/NewPlayer.js
+++ src/components/pages/NewPlayer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const NewPlayer = () => {
     return (
-        <div class="container">
-            <a href="/"><button class="back-btn"></button></a>
-            <div class="sub-container">
-                <form class="new-player--form">
-                    <input type="text" name="name" placeholder="Enter your name/nickname" class="new-player--input" minlength="2" maxlength="12" />
-                     <button class="btn save-player--btn">Save</button>
+        <div className="container">
+            <a href="/"><button className="back-btn"></button></a>
+            <div className="sub-container">
+                <form className="new-player--form">
+                    <input type="text" name="name" placeholder="Enter your name/nickname" className="new-player--input" minlength="2" maxlength="12" />
+                     <button className="btn save-player--btn">Save</button>
                  </form>
              </div>
         </div>
