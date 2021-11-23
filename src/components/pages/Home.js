c16261c0944ce05f38765930a50b1f3445dc25d3
unknown <marcin.lapinski05@gmail.com>
2021-11-23 16:09:35 +0100

    create home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,26 @@
 import React from "react";
+import img from "../../images/bronx-dice_logo.png"
 
 const Home = () => {
     return (
-        <div>Home page</div>
+        <div class="container">
+  <div class="sub-container">
+    <img src={img} alt="bronx-dice-logo" />
+    <div class="buttons-box">
+      <a href="/new-player">
+        <button class="btn nps-btn">New player</button>
+      </a>
+      <a href="/stats">
+        <button class="btn nps-btn">Statistics</button>
+      </a>
+    </div>
+    <div class="new-game-btn-box">
+        <a href="/pre-game">
+          <button class="btn ng-btn">New game</button>
+        </a>
+      </div>
+  </div>
+</div>
     );
 }
 
